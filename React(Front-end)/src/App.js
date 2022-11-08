4fa6bd07cf50e17d8a1bcf03499ff714922e91d0
VachMalkhasyan <vachmalkhasyan@gmail.com>
2022-11-08 22:08:17 +0400

    First commit

--- React(Front-end)/src/App.js
+++ React(Front-end)/src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+import Events from './pages/events';
+import Sales from './pages/sales';
+import Teams from './pages/team';
+import Blogs from './pages/blogs';
+import Support from './pages/support';
 
 function App() {
-  return (
-    <p>HELLO MY FIRST FULL PROJECT</p>
-  );
+return (
+	<Router>
+	  <Navbar />
+    <Routes>
+      <Route path='/' exact element={<Home />} />
+      <Route path='/About' element={<About />} />
+      <Route path='/Events' element={<Events />} />
+      <Route path='/Sales' element={<Sales />} />
+      <Route path='/Team' element={<Teams />} />
+      <Route path='/Blogs' element={<Blogs />} />
+      <Route path='/Support' element={<Support />} />
+    </Routes>
+	</Router>
+);
 }
 
 export default App;
---
+++ React(Front-end)/src/pages/blogs.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Blogs = () => {
+return (
+	<div
+	style={{
+		display: 'flex',
+		justifyContent: 'Right',
+		alignItems: 'Right',
+		height: '100vh'
+	}}
+	>
+	<h1>News or instagram news</h1>
+	</div>
+);
+};
+
+export default Blogs;
